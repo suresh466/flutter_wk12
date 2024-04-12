965248c57b34c9e078c4b6783a42637bc19eaf7c
Suresh <suresh.thagunna77@gmail.com>
2024-04-12 17:46:57 -0400

    init commit; create Person class

---
+++ android/app/src/main/kotlin/com/example/flutter_wk12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_wk12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/person.dart
@@ -0,0 +1,6 @@
+class Person{
+  String email;
+  String description;
+  int age;
+  Person(this.email, this.description, this.age);
+}
